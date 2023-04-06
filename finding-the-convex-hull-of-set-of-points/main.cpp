6896bd6639123d699ddc6dbd0a083ffb17f86aa6
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2023-04-07 00:19:17 +0300

    <fix>: Add =

--- finding-the-convex-hull-of-set-of-points/main.cpp
+++ finding-the-convex-hull-of-set-of-points/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <numeric>
+#include <cmath>
 
 using std::cout;
 using std::cin;
@@ -42,14 +44,18 @@ pv_t GetConvexHull(pv_t points) {
     }
     sort(points.begin() + 1,
               points.end(),
-            //лямбда-функция
-              [&points](Point a, Point b) {
-        if(sqrt(pow(a.x-points[0].x,2) + pow())
-                  return cross(
+            //лямбда-функция comparator
+              [&points](Point a, Point b) -> bool {
+        //if(sqrt(pow(a.x-points[0].x,2) + pow(a.y-points[0].y,2))
+                  return cross( // положительный поворот
                           Vector(points[0], a),
                           Vector(points[0], b)
-                  ) > 0;
-              }
+                  ) > 0
+                  || (cross( // положительный поворот
+                          Vector(points[0], a),
+                          Vector(points[0], b)
+                 ) == 0 && (sqrt(pow(a.x-points[0].x,2) + pow(a.y-points[0].y,2))<sqrt(pow(b.x-points[0].x,2) + pow(b.y-points[0].y,2))));
+        }
     );
 
     for (auto& pt: points) {
@@ -61,7 +67,7 @@ pv_t GetConvexHull(pv_t points) {
 
     for (int i = 2; i < points.size(); ++i) {
         while ((ch.size() >= 2 && cross(Vector(ch[ch.size() - 2], ch[ch.size() - 1]),
-                                       Vector(ch[ch.size() - 1], points[i])) < 0)){
+                                       Vector(ch[ch.size() - 1], points[i])) <= 0)){
             ch.pop_back();
         }
         ch.push_back(points[i]);
