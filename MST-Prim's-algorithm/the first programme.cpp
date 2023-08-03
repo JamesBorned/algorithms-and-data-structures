5b3687241fe8830abb299e4fa1100d96e6d4903d
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2023-08-03 20:35:07 +0300

    act: add a new folder

---
+++ MST-Prim's-algorithm/the first programme.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+
+int main() {
+    std::cout << "The answer to the great question about the life, \n"
+              << "The Universe and stuff like that is:"
+              << std::endl << 6 * 7 << std::endl;
+    return 0;
+}
+
+
