a96183c2adaac0cfbb0ece72e015099ed1bbc875
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2023-08-12 22:53:15 +0300

    feat: add a simply implementation of Prim's algorithm

--- MST-Prim's-algorithm/main.cpp
+++ MST-Prim's-algorithm/main.cpp
@@ -1,36 +1,72 @@
 /*Laboratory work №7
     Реализовать алгоритм Прима нахождения минимального покрывающего дерева.
-    Implement the Prim algorithm for finding a minimum spanning tree.
+    Implement Prim's algorithm for finding a minimum spanning tree.
 */
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <algorithm>
-#include <cstring>
 
 using std::cout;
 using std::cin;
 
-//#define V 5
-//#define HIGH 999999
-
-
-
 typedef std::vector<std::vector<int>>  VV_t;
 VV_t G; // graph
 VV_t MST;
-VV_t Edges;
-
 
 typedef std::vector<bool> VBOOL_t;
 VBOOL_t used; // traversed vertexes
 
 typedef std::vector<int> VINT_t;
 
-int main(){
+int minWeight = 0;
 
-    setlocale(LC_ALL,  "rus");
+int findSpanTree(int n){
+    //Initialize all positions as false initially
+    used.resize(n);
+
+    used.assign(n, false);
+
+    used[0] = true;
+
+    for (int k = 0; k < n - 1; ++k){ // E = V - 1
+//Variable to choose minimum value according to Prim's algorithm
+        int min = INT_MAX;
+
+        int numOfRow = 0, numOfColumn = 0;
+
+//Traversal along the row to pick every vertex
+        for(int i = 0; i < n;++i){
+            //After picking a vertex check if it is inside the visited array
+            if(used[i]){
+                //Traverse along columns
+                for(int j = 0;j < n; ++j){
+                    if(min > G[i][j]){
+                        if(!used[j] && G[i][j]){
+                            min = G[i][j];
+                            numOfRow = i;
+                            numOfColumn = j;
+                        }
+                    }
+                }
+            }
+        }
+
+        MST[numOfRow][numOfColumn] = G[numOfRow][numOfColumn];
+        MST[numOfColumn][numOfRow] = G[numOfRow][numOfColumn];
+
+        minWeight += G[numOfRow][numOfColumn];
+
+        used[numOfColumn]=true;
+    }
+
+    return minWeight;
+}
+
+int main() {
+
+    setlocale(LC_ALL, "rus");
 
 //Создаём файловый поток и связываем его с файлом
 //Create a file stream and link it to the file
@@ -38,12 +74,12 @@ int main(){
     std::ofstream out("..\\output.txt");
 
 
-    if(in.is_open()){
+    if (in.is_open()) {
 
         int count = 0; // the number of numbers in the file
         int temp;
 
-        while(!in.eof()){
+        while (!in.eof()) {
             in >> temp; // reading numbers from the file
             count++;
         }
@@ -58,7 +94,7 @@ int main(){
         int count_space = 0; // a number of spaces
         char symbol;
 
-        while(!in.eof()) {
+        while (!in.eof()) {
             in.get(symbol); // reading the next character
             if (symbol == ' ')
                 count_space++;
@@ -73,56 +109,52 @@ int main(){
         int tmp;
 // изменить размер вектора на n элементов типа vector<int>, каждый из которых имеет размер  n
         G.resize(n, VINT_t(n));
-        MST.resize(1,VINT_t(n));
-    /*int G[V][V] = {
-            {0,4,0,5,2},
-            {4,0,1,3,0},
-            {0,1,0,8,0},
-            {5,3,8,0,2},
-            {2,0,0,2,0}
-    };*/
-    //Array that takes care of visited vertices
-    //int visit_array[V];
+        MST.resize(n, VINT_t(n));
 
-    //Initialize all positions as false initially
-    //memset(visit_array,false,sizeof(visit_array));
-    used.assign (n, false);
-    used[0]=true;
-
-    cout<<"EDGE : WEIGHT\n";
-
-    int n_edges = 0;
-    while(n_edges<n-1){
-        //Variable to choose minimum value according to Prim's algorithm
-        int min = HIGH;
-
-        //Initialize r and c that will display the edge in output
-        int r = 0, c = 0;
-
-        //Traversal along the row to pick every vertex
-        for(int i=0; i<5;++i){
-            //After picking a vertex check if it is inside the visited array
-            if(used[i]){
-                //Now traverse along columns
-                for(int j=0;j<5;++j){
-                    if(min>G[i][j]){
-                        if(!used[j] && G[i][j]){
-                            min = G[i][j];
-                            r = i;
-                            c = j;
-                        }
-                    }
-                }
+// Reading the matrix
+        for (int i=0; i<n; ++i){
+            for (int j=0; j<n; ++j){
+                in >> G[i][j];
             }
         }
-        cout<<" "<<r<<"-"<<c<<":"<<G[r][c];
-        cout<<std::endl;
-        used[c]=true;
-        n_edges++;
+// Creating an empty graph
+        for (int i=0; i<n; ++i){
+            for (int j=0; j<n; ++j){
+                MST[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cout << G[i][j] << "\t";
+            }
+            cout << "\n";
+        }
+
+        cout << "\n";
+
+        cout << findSpanTree(n) <<'\n';
+
+        cout << "\n";
     }
+
+    for (int i = 0; i < MST.size(); ++i){
+        for (int j = 0; j < MST.size(); ++j){
+            cout << MST[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+
+// Writing to the file consist of the connected components and its number
+    if(out.is_open()) {
+        for (int i = 0; i < MST.size(); i++) {
+            for (int j = 0; j < MST[i].size(); j++) {
+                out << MST[i][j] << " ";
+            }
+            out << '\n';
+        }
+        out.close();
+    }
+
     return 0;
 }
-
-
-
-
