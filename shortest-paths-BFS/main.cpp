ff09d629b2e59dbae89113f08beefd6ff14bbe66
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2023-08-26 13:56:33 +0300

    act: add some new files

---
+++ shortest-paths-BFS/main.cpp
@@ -0,0 +1,21 @@
+/*The laboratory work №2
+ Найти в заданном графе кратчайшие пути из заданной вершины до всех
+ остальных вершин с помощью поиска в ширину.
+ Find the shortest paths from a given vertex to all other vertices
+ in a given graph, using a breadth-first search (BFS).*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+
+using std::cout;
+using std::cin;
+
+typedef std::vector<int> VINT_t;
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
