a15ff0a9e58473c9e13cbbd80ccf5453a884dceb
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2023-08-26 12:04:21 +0300

    feat: add writing the result to an output file

--- Euler-circuit/main.cpp
+++ Euler-circuit/main.cpp
@@ -23,8 +23,6 @@ VBOOL_t used;
 typedef std::vector<VINT_t> VV_t;
 VV_t G;
 
-int numOfNonZeroDegrees = 0;
-
 void Init(int n){
     used.resize(n);
     used.assign(n, false);
@@ -38,24 +36,10 @@ void DFS(int v) {
     for (int u = 0; u < G.size(); ++u) {
             // if u is not traversed and u is an adjacent to v vertex
             if (!used[u] && G[v][u] == 1) {
-                //Cycles.push_back(u);
-// Delete an edge
-//                G[v][u] = 0;
-//                G[u][v] = 0;
                 DFS(u);
             }
         }
 }
-
-//void DFS(int curr, std::vector<bool>& visited){
-//    visited[curr] = true;
-//
-//    for(auto it: adj[curr]){
-//        if(!visited[it]){
-//            DFS(it, visited);
-//        }
-//    }
-//}
 
 bool isConnected(int n){
 
@@ -63,13 +47,6 @@ bool isConnected(int n){
 
 // Node to start DFS
     int vertex = -1;
-
-//    for(int i = 0; i<V; ++i){
-//        if(adj[i].size() > 0){
-//            node = i;
-//            break;
-//        }
-//    }
 
     for (int i = 0; i < n; ++i){
         for (int j = 0; j < n; ++j){
@@ -84,9 +61,6 @@ bool isConnected(int n){
     if(vertex == -1){
         return true;
     }
-//    else {
-//        DFS(vertex);
-//    }
 
     DFS(vertex);
 
@@ -135,31 +109,10 @@ int haveEulerPath (int n) {
     }
 
     return 1;
-// Count odd-degree vertices
- //   int odd = 0;
-//    for (int i = 0; i<V; ++i){
-//// The operation AND of each bit in adj[i].size() is performed
-//// with each corresponding bit in 1
-//        if(adj[i].size() & 1)
-//            odd += 1;
-//    }
-//// Only start and end node can have odd degree
-//    if(odd > 2){
-//        return 0;
-//    }
-//// 1->Semi-Eulerian ... 2->Eulerian
-//    return (odd==0)?2:1;
 
 }
 
 void findCycle(int n, int v){
-
-    //Init(n);
-
-    //used[v] = true; // the vertex is traversed
-
-
-    //Cycles.push_back(v);
 
     for (int u = 0; u < G.size(); ++u) {
         if(Cycles[Cycles.size() - 1] != v && Degrees[v] == 2){
@@ -177,29 +130,12 @@ void findCycle(int n, int v){
             G[u][v] = 0;
 
             if (Degrees[u] % 2 != 0){
-//                int tmp = 0;
-//                for (int j = 0; j < Degrees.size();++j){
-//                    if (Degrees[j] == 0)
-//                        numOfNonZeroDegrees++;
-//                    else
-//                        cout << tmp << numOfNonZeroDegrees;
-//                        tmp = j;
-//                }
-//                if(numOfNonZeroDegrees == n-1){
-//                    Cycles.push_back(tmp);
-//                }
                 findCycle(n, u);
             }
             else{
                 if (Degrees[u] != 0) {
                     Cycles.push_back(u);
                 }
-//                for (int i = 0; i < n; ++i) {
-//                    for (int j = 0; j < n; ++j) {
-//                        cout << G[i][j] << "\t";
-//                    }
-//                    cout << "\n";
-//                }
                 break;
             }
         }
@@ -251,6 +187,14 @@ void findEuler_Path_Cycle(int n){
     }
 }
 
+void writeResultToFile(int n, std::ofstream& out){
+
+    for(int Cycle : Cycles){
+        out << Cycle << " ";
+    }
+
+}
+
 int main() {
 
     setlocale(LC_ALL, "rus");
@@ -295,7 +239,6 @@ int main() {
         int n = count_space + 1; // число столбцов
 // изменить размер вектора на n элементов типа vector<int>, каждый из которых имеет размер  n
         G.resize(n, VINT_t(n));
-        //Degrees.resize(n);
 
 // Reading the matrix
         for (int i = 0; i < n; ++i) {
@@ -311,24 +254,17 @@ int main() {
             cout << "\n";
         }
 
-//    adj[0].push_back(1);
-//    adj[1].push_back(0);
-//    adj[0].push_back(2);
-//    adj[2].push_back(0);
-//    adj[2].push_back(1);
-//    adj[1].push_back(2);
-//    adj[0].push_back(3);
-//    adj[3].push_back(0);
-//    adj[3].push_back(4);
-//    adj[4].push_back(3);
-//    findEuler_Path_Cycle();
-
     findEuler_Path_Cycle(n);
 
-        for(int i = 0; i < Cycles.size(); ++i){
-            cout << Cycles[i] << '\n';
-        }
+//        for(int i = 0; i < Cycles.size(); ++i){
+//            cout << Cycles[i] << '\n';
+//        }
+
+        writeResultToFile(n, out);
+
+        in.close();
+
+    }
 
         return 0;
-    }
 }
